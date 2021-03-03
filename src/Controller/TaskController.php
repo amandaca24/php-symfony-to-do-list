2d64141433ca41fc1eff18b9dbcb3b28bfc39b9c
amandacarvalho24 <amandacarvalho24@gmail.com>
2021-03-03 11:33:06 -0300

    to-do-list-symfony
    #Second Commit

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TaskRepository;
+use App\Entity\Task;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class TaskController extends AbstractController
+{
+    /**
+     * @Route("/tasks", name="tasks_index")
+     */
+    public function index(TaskRepository $taskRespository): Response
+    {
+        return $this->render('task/index.html.twig', [
+            'tasks' => $taskRespository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="task_new")
+     */
+    public function createTask(ValidatorInterface $validator, Request $request): Response
+    {
+        if($request->isMethod('post')){
+            
+            $task = new Task();
+
+            $task->setTitle($request->request->get('title'));
+            $task->setDate($request->request->get('date'));
+            $task->setDone($request->request->get('done'));
+
+            $errors = $validator->validate($task);
+
+            if (count($errors) > 0) {
+                return $this->render('task/form.html.twig', [
+                    'errors' => $errors,
+                ]);
+            }
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($task);
+            $entityManager->flush(); 
+            
+            return $this->redirectToRoute('tasks_index');
+
+
+        }
+
+        return $this->render('task/form.html.twig', [
+            'errors' => '',
+        ]);
+    } 
+
+    /**
+     * @Route("/task/edit/{id}", name="task_edit")
+     */
+    public function updateTask(ValidatorInterface $validator, Request $request, int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        
+        $task = $entityManager->getRepository(Task::class)->find($id);
+
+
+        if (!$task) {
+            throw $this->addFlash('error', 'There is no task with ' .$id);
+        }
+
+        if($request->isMethod('post')){
+
+            $errors = $validator->validate($task);
+
+            if (count($errors) > 0) {
+                return $this->render('task/form.html.twig', [
+                    'errors' => $errors,
+                ]);
+            }
+
+            $task->setTitle($request->request->get('title'));
+            $task->setDate($request->request->get('date'));
+            $task->setDone($request->request->get('done'));
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            $this->addFlash('sucess', 'Task saved!');
+
+            return $this->redirectToRoute('tasks_index');
+
+        }
+
+        return $this->render('task/edit.html.twig', [
+            'id' => $id,
+            'task' => $task,
+            'errors' => ''
+        ]);  
+    }
+
+    /**
+     * @Route("/task/remove/{id}", name="task_delete")
+     */
+    public function deleteTask(Request $request, int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        
+        $task = $entityManager->getRepository(Task::class)->find($id);
+
+
+        if (!$task) {
+            throw $this->addFlash('error', 'There is no task with ' .$id);
+        }
+
+        $entityManager->remove($task);
+        $entityManager->flush();
+
+        $this->addFlash('delete', 'Task deleted!');
+
+        return $this->redirectToRoute('tasks_index');
+
+    }
+
+
+}
---
+++ src/Entity/Task.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=TaskRepository::class)
+ */
+class Task
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+ 
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Title must not be empty")
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank(message="Date must not be empty")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @Assert\NotNull(message="You must assign a status for the task")
+     */
+    private $done;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDate(): ?string
+    {
+        return $this->date;
+    }
+
+    public function setDate(string $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getDone(): ?bool
+    {
+        return $this->done;
+    }
+
+    public function setDone(bool $done): self
+    {
+        $this->done = $done;
+
+        return $this;
+    }
+}
